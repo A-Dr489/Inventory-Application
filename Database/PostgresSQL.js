71215f03a1e0ad61a0d35d09e3c83b07cec38205
Asan Sabir <asansaber123321@gmail.com>
2025-08-10 21:17:24 +0300

    changed the database connection

--- Database/PostgresSQL.js
+++ Database/PostgresSQL.js
@@ -1,9 +1,8 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: Number(process.env.PORTM)
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
